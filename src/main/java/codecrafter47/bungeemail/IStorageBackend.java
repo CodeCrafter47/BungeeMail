bc658a95ac43eed2674ea0b87810512e4d01b1b0
Florian Stober <Flo.e3@gmx.de>
2015-07-06 12:40:51 +0200

    remove public on interface methods

--- src/main/java/codecrafter47/bungeemail/IStorageBackend.java
+++ src/main/java/codecrafter47/bungeemail/IStorageBackend.java
@@ -8,20 +8,20 @@ import java.util.UUID;
  * Created by florian on 15.11.14.
  */
 public interface IStorageBackend {
-    public List<Message> getMessagesFor(UUID uuid, boolean onlyNew);
+    List<Message> getMessagesFor(UUID uuid, boolean onlyNew);
 
-    public void saveMessage(Message message);
+    void saveMessage(Message message);
 
-    public void markRead(Message message);
+    void markRead(Message message);
 
-    public void delete(Message message);
+    void delete(Message message);
 
-    public UUID getUUIDForName(String name);
+    UUID getUUIDForName(String name);
 
-    public Collection<UUID> getAllKnownUUIDs();
+    Collection<UUID> getAllKnownUUIDs();
 
     // used for tab-complete
-    public Collection<String> getKnownUsernames();
+    Collection<String> getKnownUsernames();
 
     void delete(int id);
 
