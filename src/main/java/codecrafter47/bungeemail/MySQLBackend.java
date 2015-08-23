ec3164b5bee8d16fcc2c6abe8cbbda78d860f1e8
Florian Stober <Flo.e3@gmx.de>
2015-08-23 16:29:21 +0200

    use a mysql connection pool

--- src/main/java/codecrafter47/bungeemail/MySQLBackend.java
+++ src/main/java/codecrafter47/bungeemail/MySQLBackend.java
@@ -1,31 +1,28 @@
 package codecrafter47.bungeemail;
 
 import com.google.common.base.Preconditions;
-import lib.PatPeter.SQLibrary.Database;
-import lib.PatPeter.SQLibrary.MySQL;
+import org.apache.commons.dbcp2.*;
+import org.apache.commons.pool2.ObjectPool;
+import org.apache.commons.pool2.impl.GenericObjectPool;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import javax.sql.DataSource;
+import java.beans.*;
+import java.sql.*;
+import java.sql.Statement;
 import java.util.*;
 
 public class MySQLBackend implements IStorageBackend {
 
     BungeeMail plugin;
-    Database sql;
+    DataSource dataSource;
 
     public MySQLBackend(BungeeMail plugin) {
         this.plugin = plugin;
-        sql = new MySQL(plugin.getLogger(), "[BungeeMail/MySQL]", plugin.config.getString("mysql_hostname"),
-                plugin.config.getInt("mysql_port"), plugin.config.getString("mysql_database"),
-                plugin.config.getString("mysql_username"), plugin.config.getString("mysql_password"));
-        try {
-            sql.open();
-            if (sql.isOpen()) {
-                sql.query("CREATE TABLE IF NOT EXISTS bungeemail_mails (id int NOT NULL AUTO_INCREMENT,senderName varchar(20), senderUUID varchar(40), recipient varchar(40), `message` varchar(255), `read` boolean, `time` bigint, PRIMARY KEY (id))");
-                sql.query("CREATE TABLE IF NOT EXISTS bungeemail_uuids (id int NOT NULL AUTO_INCREMENT,username varchar(20), uuid varchar(40),PRIMARY KEY (id))");
-            } else {
-                throw new RuntimeException("Failed to connect to MySql database");
+        setupDataSource(plugin);
+        try (Connection connection = dataSource.getConnection()) {
+            try(Statement statement = connection.createStatement()){
+                statement.execute("CREATE TABLE IF NOT EXISTS bungeemail_mails (id int NOT NULL AUTO_INCREMENT,senderName varchar(20), senderUUID varchar(40), recipient varchar(40), `message` varchar(255), `read` boolean, `time` bigint, PRIMARY KEY (id))");
+                statement.execute("CREATE TABLE IF NOT EXISTS bungeemail_uuids (id int NOT NULL AUTO_INCREMENT,username varchar(20), uuid varchar(40),PRIMARY KEY (id))");
             }
         } catch (SQLException e) {
             plugin.getLogger().warning("MySQL setup failed");
@@ -33,33 +30,46 @@ public class MySQLBackend implements IStorageBackend {
         }
     }
 
+    private void setupDataSource(BungeeMail plugin) {
+        try {
+            Class.forName("com.mysql.jdbc.jdbc2.optional.MysqlDataSource");
+        } catch (ClassNotFoundException var2) {
+            plugin.getLogger().warning("MySQL DataSource class missing: " + var2.getMessage() + ".");
+            throw new RuntimeException("Failed to connect to MySql database");
+        }
+        ConnectionFactory connectionFactory = new DriverManagerConnectionFactory("jdbc:mysql://" + plugin.config.getString("mysql_hostname") + ":" + plugin.config.getInt("mysql_port") + "/" + plugin.config.getString("mysql_database"), plugin.config.getString("mysql_username"), plugin.config.getString("mysql_password"));
+        PoolableConnectionFactory poolableConnectionFactory = new PoolableConnectionFactory(connectionFactory, null);
+        ObjectPool<PoolableConnection> connectionPool = new GenericObjectPool<>(poolableConnectionFactory);
+        poolableConnectionFactory.setPool(connectionPool);
+        this.dataSource = new PoolingDataSource<>(connectionPool);
+    }
+
     @Override
     public List<Message> getMessagesFor(UUID uuid, boolean onlyNew) throws StorageException {
-        try {
-            if (!sql.isOpen()) {
-                sql.open();
-            }
-            PreparedStatement ps;
-            ResultSet rs;
+        try (Connection connection = dataSource.getConnection()){
+            String sql;
             if (onlyNew) {
-                ps = sql.prepare("select * from bungeemail_mails where recipient=? and `read`='0'");
+                sql = "select * from bungeemail_mails where recipient=? and `read`='0'";
             } else {
-                ps = sql.prepare("select * from bungeemail_mails where recipient=?");
+                sql = "select * from bungeemail_mails where recipient=?";
             }
-            ps.setString(1, uuid.toString());
-            rs = sql.query(ps);
-            ArrayList<Message> messages = new ArrayList<>();
-            while (rs.next()) {
-                String senderName = rs.getString("senderName");
-                UUID senderUUID = UUID.fromString(rs.getString("senderUUID"));
-                UUID recipient = UUID.fromString(rs.getString("recipient"));
-                String message = rs.getString("message");
-                boolean read = rs.getBoolean("read");
-                long time = rs.getLong("time");
-                long id = rs.getLong("id");
-                messages.add(new SQLMessage(id, senderName, senderUUID, recipient, message, read, time));
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                ps.setString(1, uuid.toString());
+                try (ResultSet rs = ps.executeQuery()) {
+                    ArrayList<Message> messages = new ArrayList<>();
+                    while (rs.next()) {
+                        String senderName = rs.getString("senderName");
+                        UUID senderUUID = UUID.fromString(rs.getString("senderUUID"));
+                        UUID recipient = UUID.fromString(rs.getString("recipient"));
+                        String message = rs.getString("message");
+                        boolean read = rs.getBoolean("read");
+                        long time = rs.getLong("time");
+                        long id = rs.getLong("id");
+                        messages.add(new SQLMessage(id, senderName, senderUUID, recipient, message, read, time));
+                    }
+                    return messages;
+                }
             }
-            return messages;
         } catch (SQLException e) {
             throw new StorageException(e);
         }
@@ -67,22 +77,28 @@ public class MySQLBackend implements IStorageBackend {
 
     @Override
     public Message saveMessage(String senderName, UUID senderUUID, UUID recipient, String message, boolean read, long time) throws StorageException {
-        try {
-            if (!sql.isOpen()) {
-                sql.open();
+        try (Connection connection = dataSource.getConnection()){
+            try (PreparedStatement ps = connection.prepareStatement("insert into bungeemail_mails values(NULL, ?, ?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS)) {
+                ps.setString(1, senderName);
+                ps.setString(2, senderUUID.toString());
+                ps.setString(3, recipient.toString());
+                ps.setString(4, message);
+                ps.setBoolean(5, read);
+                ps.setLong(6, time);
+                int affectedRows = ps.executeUpdate();
+                if(affectedRows == 1) {
+                    try (ResultSet rs = ps.getGeneratedKeys()) {
+                        if(rs.next()){
+                            long id = rs.getLong(1);
+                            return new SQLMessage(id, senderName, senderUUID, recipient, message, read, time);
+                        } else {
+                            throw new StorageException("Saving mail failed. Generated key not available");
+                        }
+                    }
+                } else {
+                    throw new StorageException("Saving mail failed. " + affectedRows + " rows inserted. But should be 1");
+                }
             }
-            PreparedStatement ps = sql.prepare("insert into bungeemail_mails values(NULL, ?, ?, ?, ?, ?, ?)");
-            ps.setString(1, senderName);
-            ps.setString(2, senderUUID.toString());
-            ps.setString(3, recipient.toString());
-            ps.setString(4, message);
-            ps.setBoolean(5, read);
-            ps.setLong(6, time);
-            ArrayList<Long> ids = sql.insert(ps);
-            if(ids.size() != 1){
-                throw new StorageException("Saving mail failed. " + ids.size() + " rows inserted. But should be 1");
-            }
-            return new SQLMessage(ids.get(0), senderName, senderUUID, recipient, message, read, time);
         } catch (SQLException e) {
             throw new StorageException(e);
         }
@@ -91,14 +107,14 @@ public class MySQLBackend implements IStorageBackend {
     @Override
     public void markRead(Message message) throws StorageException {
         Preconditions.checkArgument(message instanceof SQLMessage);
-        try {
-            if (!sql.isOpen()) {
-                sql.open();
+        try (Connection connection = dataSource.getConnection()){
+            try(PreparedStatement ps = connection.prepareStatement("update bungeemail_mails set `read`=1 where id=?")) {
+                ps.setLong(1, message.hashCode());
+                if(ps.executeUpdate() == 0){
+                    throw new StorageException("Tried to read non-existent mail");
+                }
+                ((SQLMessage) message).setRead(true);
             }
-            PreparedStatement ps = sql.prepare("update bungeemail_mails set `read`=1 where id=?");
-            ps.setLong(1, message.hashCode());
-            sql.query(ps);
-            ((SQLMessage)message).setRead(true);
         } catch (SQLException e) {
             throw new StorageException(e);
         }
@@ -111,13 +127,13 @@ public class MySQLBackend implements IStorageBackend {
 
     @Override
     public void delete(long id) throws StorageException {
-        try {
-            if (!sql.isOpen()) {
-                sql.open();
+        try (Connection connection = dataSource.getConnection()){
+            try (PreparedStatement ps = connection.prepareStatement("delete from bungeemail_mails where id=?")) {
+                ps.setLong(1, id);
+                if(ps.executeUpdate() == 0){
+                    throw new StorageException("Tried to delete non-existent mail");
+                }
             }
-            PreparedStatement ps = sql.prepare("delete from bungeemail_mails where id=?");
-            ps.setLong(1, id);
-            sql.query(ps);
         } catch (SQLException e) {
             throw new StorageException(e);
         }
@@ -125,18 +141,17 @@ public class MySQLBackend implements IStorageBackend {
 
     @Override
     public void deleteOlder(long time, boolean deleteUnread) throws StorageException {
-        try {
-            if (!sql.isOpen()) {
-                sql.open();
-            }
-            PreparedStatement ps;
+        try (Connection connection = dataSource.getConnection()){
+            String sql;
             if (deleteUnread) {
-                ps = sql.prepare("delete from bungeemail_mails where time < ?");
+                sql = "delete from bungeemail_mails where time < ?";
             } else {
-                ps = sql.prepare("delete from bungeemail_mails where time < ? and `read`=0");
+                sql = "delete from bungeemail_mails where time < ? and `read`=0";
             }
-            ps.setLong(1, time);
-            sql.query(ps);
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                ps.setLong(1, time);
+                ps.executeUpdate();
+            }
         } catch (SQLException e) {
             throw new StorageException(e);
         }
@@ -144,17 +159,16 @@ public class MySQLBackend implements IStorageBackend {
 
     @Override
     public UUID getUUIDForName(String name) throws StorageException {
-        try {
-            if (!sql.isOpen()) {
-                sql.open();
+        try (Connection connection = dataSource.getConnection()){
+            try (PreparedStatement ps = connection.prepareStatement("select uuid from bungeemail_uuids where username=?")) {
+                ps.setString(1, name);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return UUID.fromString(rs.getString("uuid"));
+                    }
+                    return null;
+                }
             }
-            PreparedStatement ps = sql.prepare("select * from bungeemail_uuids where username=?");
-            ps.setString(1, name);
-            ResultSet rs = sql.query(ps);
-            if (rs.next()) {
-                return UUID.fromString(rs.getString("uuid"));
-            }
-            return null;
         } catch (SQLException e) {
             throw new StorageException(e);
         }
@@ -162,16 +176,16 @@ public class MySQLBackend implements IStorageBackend {
 
     @Override
     public Collection<UUID> getAllKnownUUIDs() throws StorageException {
-        try {
-            if (!sql.isOpen()) {
-                sql.open();
+        try (Connection connection = dataSource.getConnection()){
+            try(Statement statement = connection.createStatement()) {
+                try (ResultSet rs = statement.executeQuery("select uuid from bungeemail_uuids")) {
+                    Collection<UUID> uuids = new ArrayList<>();
+                    while (rs.next()) {
+                        uuids.add(UUID.fromString(rs.getString("uuid")));
+                    }
+                    return uuids;
+                }
             }
-            HashSet<UUID> uuids = new HashSet<>();
-            ResultSet rs = sql.query("select * from bungeemail_uuids");
-            while (rs.next()) {
-                uuids.add(UUID.fromString(rs.getString("uuid")));
-            }
-            return uuids;
         } catch (SQLException e) {
             throw new StorageException(e);
         }
@@ -179,16 +193,16 @@ public class MySQLBackend implements IStorageBackend {
 
     @Override
     public Collection<String> getKnownUsernames() throws StorageException {
-        try {
-            if (!sql.isOpen()) {
-                sql.open();
+        try (Connection connection = dataSource.getConnection()){
+            try(Statement statement = connection.createStatement()) {
+                try (ResultSet rs = statement.executeQuery("select username from bungeemail_uuids")) {
+                    Collection<String> names = new ArrayList<>();
+                    while (rs.next()) {
+                        names.add(rs.getString("username"));
+                    }
+                    return names;
+                }
             }
-            HashSet<String> strings = new HashSet<>();
-            ResultSet rs = sql.query("select * from bungeemail_uuids");
-            while (rs.next()) {
-                strings.add(rs.getString("username"));
-            }
-            return strings;
         } catch (SQLException e) {
             throw new StorageException(e);
         }
@@ -196,27 +210,23 @@ public class MySQLBackend implements IStorageBackend {
 
     @Override
     public void updateUserEntry(final UUID uuid, final String username) throws StorageException {
-        try {
-            if (!sql.isOpen()) {
-                sql.open();
+        try (Connection connection = dataSource.getConnection()){
+            try(PreparedStatement ps = connection.prepareStatement("insert into bungeemail_uuids values(NULL, ?, ?)", Statement.RETURN_GENERATED_KEYS)){
+                ps.setString(1, username);
+                ps.setString(2, uuid.toString());
+                if(ps.executeUpdate() == 1){
+                    try (ResultSet key = ps.getGeneratedKeys()){
+                        if(key.next()) {
+                            long id = key.getLong(1);
+                            try (PreparedStatement ps2 = connection.prepareStatement("delete from bungeemail_uuids where uuid = ? and id != ?", Statement.RETURN_GENERATED_KEYS)) {
+                                ps2.setString(1, uuid.toString());
+                                ps2.setLong(2, id);
+                                ps2.executeUpdate();
+                            }
+                        }
+                    }
+                }
             }
-            PreparedStatement ps = sql.prepare("select * from bungeemail_uuids where uuid=?");
-            ps.setString(1, uuid.toString());
-            ResultSet rs = sql.query(ps);
-            boolean upToDate = false;
-            boolean there = false;
-            while (rs.next()) {
-                there = true;
-                upToDate = rs.getString("username").equals(username);
-            }
-            if (!there) {
-                ps = sql.prepare("insert into bungeemail_uuids values(NULL, ?, ?)");
-            } else if (!upToDate) {
-                ps = sql.prepare("update bungeemail_uuids set username=? where uuid=?");
-            }
-            ps.setString(1, username);
-            ps.setString(2, uuid.toString());
-            sql.query(ps);
         } catch (SQLException e) {
             throw new StorageException(e);
         }
@@ -287,7 +297,7 @@ public class MySQLBackend implements IStorageBackend {
 
         @Override
         public boolean equals(Object other) {
-            return other instanceof SQLMessage && ((SQLMessage)other).getId() == getId();
+            return other instanceof SQLMessage && ((SQLMessage) other).getId() == getId();
         }
     }
 }
